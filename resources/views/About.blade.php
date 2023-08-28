334d50c7834c0c5181b181b91d65abdf8bf3394c
ahmadgul01 <muhammadahmadgul007@gmail.com>
2023-08-28 15:00:02 +0500

    sec commit

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @push('bb')
-    <title>About</title>
+    <title>About - Page</title>
     @endpush
 </head>
 <body>
